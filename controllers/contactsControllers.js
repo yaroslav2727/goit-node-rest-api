f01ddbb395615cb6506fa898d90601a815959b46
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2024-05-14 20:39:36 +0300

    homework

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,57 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (_, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const result = await contactsService.getContactById(req.params.id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  try {
+    const result = await contactsService.updateContact(req.params.id, req.body);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -6,6 +6,9 @@ import {
   createContact,
   updateContact,
 } from "../controllers/contactsControllers.js";
+import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { isEmptyBody } from "../middlewares/isEmptyBody.js";
 
 const contactsRouter = express.Router();
 
@@ -15,8 +18,8 @@ contactsRouter.get("/:id", getOneContact);
 
 contactsRouter.delete("/:id", deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post("/", validateBody(createContactSchema), createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put("/:id", isEmptyBody, validateBody(updateContactSchema), updateContact);
 
 export default contactsRouter;
